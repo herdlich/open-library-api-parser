a09b84ede49a28a0eeb8675f9b94a45e6e31f1ad
herdlich <herdlich@gmail.com>
2026-07-09 16:04:09 +0300

    fix: ensure SQLite connections are closed safely

--- src/open_library_parser/database.py
+++ src/open_library_parser/database.py
@@ -1,37 +1,35 @@
 import sqlite3
 from pathlib import Path
+from contextlib import closing
 
 
 def db_init(db_file):
     Path(db_file).parent.mkdir(parents=True, exist_ok=True)
 
     connection = sqlite3.connect(db_file)
-    cursor = connection.cursor()
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS books (
-        book_id TEXT PRIMARY KEY,
-        title TEXT,
-        author_name TEXT,
-        first_publish_year INTEGER,
-        language TEXT,
-        edition_count INTEGER,
-        cover_i INTEGER,
-        cover_link TEXT,
-        ia TEXT,
-        link TEXT
-        )
-    """)
+    with closing(connection) as db:
+        with db:
+            cursor = db.cursor()
 
-    connection.commit()
-    connection.close()
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS books (
+                book_id TEXT PRIMARY KEY,
+                title TEXT,
+                author_name TEXT,
+                first_publish_year INTEGER,
+                language TEXT,
+                edition_count INTEGER,
+                cover_i INTEGER,
+                cover_link TEXT,
+                ia TEXT,
+                link TEXT
+                )
+            """)
 
 
 def save_to_db(db_file, items):
     Path(db_file).parent.mkdir(parents=True, exist_ok=True)
-
-    connection = sqlite3.connect(db_file)
-    cursor = connection.cursor()
 
     rows = [
         (
@@ -49,21 +47,24 @@ def save_to_db(db_file, items):
         for item in items
     ]
 
-    cursor.executemany("""
-    INSERT INTO books (book_id, title, author_name, first_publish_year, language, 
-    edition_count, cover_i, cover_link, ia, link)
-    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ON CONFLICT(book_id) DO UPDATE SET
-    title = excluded.title,
-    author_name = excluded.author_name,
-    first_publish_year = excluded.first_publish_year,
-    language = excluded.language,
-    edition_count = excluded.edition_count,
-    cover_i = excluded.cover_i,
-    cover_link = excluded.cover_link,
-    ia = excluded.ia,
-    link = excluded.link
-    """, rows)
+    connection = sqlite3.connect(db_file)
 
-    connection.commit()
-    connection.close()
+    with closing(connection) as db:
+        with db:
+            cursor = db.cursor()
+
+            cursor.executemany("""
+            INSERT INTO books (book_id, title, author_name, first_publish_year, language, 
+            edition_count, cover_i, cover_link, ia, link)
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ON CONFLICT(book_id) DO UPDATE SET
+            title = excluded.title,
+            author_name = excluded.author_name,
+            first_publish_year = excluded.first_publish_year,
+            language = excluded.language,
+            edition_count = excluded.edition_count,
+            cover_i = excluded.cover_i,
+            cover_link = excluded.cover_link,
+            ia = excluded.ia,
+            link = excluded.link
+            """, rows)
